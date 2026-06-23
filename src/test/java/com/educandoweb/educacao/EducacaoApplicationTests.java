801b413c921d2e7b7db6a17484e96736d1df441b
“Lucas <lucashenrique125@hotmail.com>
2026-06-23 07:37:23 -0400

    Project created

---
+++ src/test/java/com/educandoweb/educacao/EducacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.educacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
